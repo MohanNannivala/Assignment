1057c53d268f360be2dae4c797bac252344f0b73
MohanNannivala <mohannannivala@yahoo.com>
2022-08-02 01:10:18 +0530

    Optimized the algorithms time complexity to O(N log(log N)) by using extra space of O(N).

--- SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SumOfPrimeNumberService.java
+++ SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SumOfPrimeNumberService.java
@@ -2,6 +2,8 @@ package com.sumprimenumber.service;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public class SumOfPrimeNumberService {
 
@@ -9,20 +11,23 @@ public class SumOfPrimeNumberService {
 
         long result=0;
 
-        for(int i=2; i<=number; i++){
+        ArrayList<Boolean> isPrime=new ArrayList<>();
 
-            boolean isPrime=true;
+        for(int i=0; i<=number; i++){
+            isPrime.add(true);
+        }
 
-            for(int j=2; j<i; j++){
-                if(i%j == 0 ){
-                    isPrime=false;
-                    break;
+        for(int i=2; i<=Math.sqrt(number); i++){
+            if(isPrime.get(i)){
+                for(int j=i*i; j<=number; j+=i){
+                    isPrime.set(j, false);
                 }
             }
+        }
 
-            if(isPrime){
-                result +=i;
-                isPrime=true;
+        for(int i=2; i<=number; i++){
+            if(isPrime.get(i)){
+                result+=i;
             }
         }
 
