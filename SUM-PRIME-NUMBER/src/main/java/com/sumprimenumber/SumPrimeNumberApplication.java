1863c5f4eb384ba12904591ab10d16c638e2659d
MohanNannivala <mohannannivala@yahoo.com>
2022-08-01 22:17:45 +0530

    Base version

---
+++ SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/SumPrimeNumberApplication.java
@@ -0,0 +1,13 @@
+package com.sumprimenumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumPrimeNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumPrimeNumberApplication.class, args);
+    }
+
+}
---
+++ SUM-PRIME-NUMBER/src/test/java/com/sumprimenumber/SumPrimeNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumprimenumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumPrimeNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
