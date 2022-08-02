b85fbdc5b0900c21468890005ea7708b1796d30a
MohanNannivala <mohannannivala@yahoo.com>
2022-08-03 01:50:16 +0530

    Rename the variable number to limit in service package

--- SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SieveAlgorithm.java
+++ SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SieveAlgorithm.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class SieveAlgorithm {
 
-    public void createSieveArray(long number, ArrayList<Integer> primeNumbers){
+    public void createSieveArray(long limit, ArrayList<Integer> primeNumbers){
 
         ArrayList<Boolean> isPrime=new ArrayList<>();
 
@@ -12,16 +12,16 @@ public class SieveAlgorithm {
          * To identify the primes in from "0" to "number" ,
          * ith index will be false if it is not a prime number else true
          */
-        for(int i = 0; i<=number; i++){
+        for(int i = 0; i<=limit; i++){
             isPrime.add(true);
         }
 
-        for(int i=2; i*i<=number; i++){
+        for(int i=2; i*i<=limit; i++){
             if(isPrime.get(i)){
 
                 //set all multiples of i which are not prime (isPrime[j] = false)
 
-                for(int j=i*i; j<=number; j+=i){
+                for(int j=i*i; j<=limit; j+=i){
                     isPrime.set(j, false);
                 }
             }
--- SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SumOfPrimeNumberService.java
+++ SUM-PRIME-NUMBER/src/main/java/com/sumprimenumber/service/SumOfPrimeNumberService.java
@@ -12,38 +12,38 @@ import static java.lang.Math.sqrt;
 @Component
 public class SumOfPrimeNumberService{
 
-    public long calculateSumOfPrimeNumbers(long number){
+    public long calculateSumOfPrimeNumbers(long limit){
 
         //If the number is not in between 2 and 10 million user will get the below exception
-        if (number < Range.LOW_LIMIT || number > Range.HIGH_LIMIT) {
+        if (limit < Range.LOW_LIMIT || limit > Range.HIGH_LIMIT) {
             throw new InvalidInputException("Input number is invalid, please provide input between " + Range.LOW_LIMIT + " and " + Range.HIGH_LIMIT);
         }
 
         long result = 0L;
         // Compute all primes smaller than or equal to square root of number using sieveAlgorithm
-        int limit = (int) (floor(sqrt(number))+1);
+        int range = (int) (floor(sqrt(limit))+1);
         ArrayList<Integer> primeNumbers=new ArrayList<>();
         SieveAlgorithm sieveAlgorithm=new SieveAlgorithm();
-        sieveAlgorithm.createSieveArray(limit, primeNumbers);
+        sieveAlgorithm.createSieveArray(range, primeNumbers);
 
         for(int i : primeNumbers){
             result+=i;
         }
 
         // Divide the range in different segments We have chosen segment size as sqrt(n).
-        int low  = limit;
-        int high = 2*limit;
+        int low  = range;
+        int high = 2*range;
 
         SegmentedSieveAlgorithm segmentedSieveAlgorithm=new SegmentedSieveAlgorithm();
 
         //calculate the sum of all the prime numbers between low and high, segment by segment using segmented sieve algorithm.
-        while(low<number){
+        while(low<limit){
 
-            result += segmentedSieveAlgorithm.createSieveArray(low, (int)Math.min(number, high), primeNumbers);
+            result += segmentedSieveAlgorithm.createSieveArray(low, (int)Math.min(limit, high), primeNumbers);
 
             //Update low and high for next segment
-            low  = low + limit;
-            high = high + limit;
+            low  = low + range;
+            high = high + range;
         }
 
         return result;
